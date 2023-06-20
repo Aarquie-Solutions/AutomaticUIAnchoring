aa1517f08be55d300a1e399382868fcaf39feb2b
Aarquie <70454750+Aarquie@users.noreply.github.com>
2023-06-20 01:21:17 -0400

    Imported the script

---
+++ Editor/AutomaticUIAnchoringTool.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace AarquieSolutions.AutomaticUIAnchoring.Editor
+{
+    public class AutomaticUIAnchoringTool : UnityEditor.Editor
+    {
+        private static void Anchor(RectTransform rectTransform)
+        {
+            RectTransform parentRectTransform = null;
+            if (rectTransform.transform.parent)
+            {
+                parentRectTransform = rectTransform.transform.parent.GetComponent<RectTransform>();
+            }
+
+            if (!parentRectTransform)
+            {
+                return;
+            }
+
+            Undo.RecordObject(rectTransform, "Anchor UI Object");
+            Rect parentRect = parentRectTransform.rect;
+            rectTransform.anchorMin = new Vector2(rectTransform.anchorMin.x + (rectTransform.offsetMin.x / parentRect.width),
+                rectTransform.anchorMin.y + (rectTransform.offsetMin.y / parentRect.height));
+            rectTransform.anchorMax = new Vector2(rectTransform.anchorMax.x + (rectTransform.offsetMax.x / parentRect.width),
+                rectTransform.anchorMax.y + (rectTransform.offsetMax.y / parentRect.height));
+            rectTransform.offsetMin = Vector2.zero;
+            rectTransform.offsetMax = Vector2.zero;
+            rectTransform.pivot = new Vector2(0.5f, 0.5f);
+        }
+
+        [MenuItem("Tools/Automatic UI Anchoring/Anchor Selected UI Objects")]
+        private static void AnchorSelectedUIObjects()
+        {
+            foreach (GameObject t in Selection.gameObjects)
+            {
+                RectTransform rectTransform = t.GetComponent<RectTransform>();
+                if (rectTransform)
+                {
+                    Anchor(rectTransform);
+                }
+            }
+        }
+    }
+}
